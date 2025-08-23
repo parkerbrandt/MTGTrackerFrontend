9f939220652ab54ee7a4a3155f67a826f1a293d3
Parker Brandt <parker.brandt101@gmail.com>
2025-08-23 15:31:36 -0500

    Added new files and modified home page

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <div>
+            <button>Home</button>
+            <button>Profile</button>
+            <button>Decks</button>
+            <button>Groups</button>
+            <button>Match History</button>
+            <button>Log</button>
+        </div>
+    );
+}
+
+export default Header;
