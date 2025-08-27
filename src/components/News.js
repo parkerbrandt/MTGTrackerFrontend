24dd7aa3169948a60fe6585f088596092a41b165
Parker Brandt <parker.brandt101@gmail.com>
2025-08-26 20:36:05 -0500

    Added some files to create components with

---
+++ src/components/News.js
@@ -0,0 +1,9 @@
+function News() {
+    return (
+        <div>
+            <h3>News</h3>
+        </div>
+    );
+}
+
+export default News;
---
+++ src/pages/Log/Log.js
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+
+
+const Log = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Log</h1>
+        </div>
+    );
+}
+
+export default Log;
