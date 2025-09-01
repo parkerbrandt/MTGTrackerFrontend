02a27f94f66f43c4d0bcbcac26384e3092b7914c
Parker Brandt <parker.brandt101@gmail.com>
2025-09-01 00:05:58 -0500

    Added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import './footer.css'
+
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <label>Contact Us</label>
+        </div>
+    );
+}
+
+export default Footer;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,9 @@
 import './home.css'
 
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import News from '../../components/News';
+
 
 const Home = () => {
   return (
@@ -9,8 +11,10 @@ const Home = () => {
       <Header /><br />
       <h1 className="welcome">Track Your Magic Games!</h1><br />
       <News />
+      <h3>Featured</h3>
       <h3>Your Updates</h3>
-      <h3>Looking for Players</h3>
+      <h3>Looking for Players</h3><br />
+      <Footer />
     </div>
   );
 }
