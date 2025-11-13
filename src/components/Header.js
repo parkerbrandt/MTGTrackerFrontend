0d17ba40188896767fafa8cc8adc6018a370d398
Parker Brandt <parker.brandt101@gmail.com>
2025-11-13 15:28:16 -0600

    Added missing imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import './header.css'
 
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
